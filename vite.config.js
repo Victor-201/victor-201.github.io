7f3bde8f8a281f39c0e6a373bb4e1fb2960e560d
Victor-201 <4.victor.201@gmail.com>
2026-06-10 16:16:30 +0700

    remove: delete legacy JSX project files and configs

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Config base để deploy GitHub Pages
-export default defineConfig({
-  base: "/victor-201.github.io",
-  plugins: [react()],
-})
